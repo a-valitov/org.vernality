3ead2a8d3ec6fa3779800216ebb04a96670173f2
Rinat Enikeev <rinat.enikeev@gmail.com>
2020-11-22 14:57:13 +0500

    use container, not self.view

--- Projects/ProfitClub/iOS/ProfitClub/Presentation/Onboard/View/OnboardSignUp/Alpha/OnboardSignUpViewAlpha.swift
+++ Projects/ProfitClub/iOS/ProfitClub/Presentation/Onboard/View/OnboardSignUp/Alpha/OnboardSignUpViewAlpha.swift
@@ -151,7 +151,7 @@ extension OnboardSignUpViewAlpha {
     private func layoutRegistrationLabel(in container: UIView) {
         let label = registrationLabel
         label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
+        container.addSubview(label)
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: container.topAnchor, constant: 8),
             label.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20)
@@ -164,13 +164,12 @@ extension OnboardSignUpViewAlpha {
         stack.axis = .vertical
         stack.distribution = .fill
         stack.spacing = 15
-        view.addSubview(stack)
+        container.addSubview(stack)
         stack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             stack.topAnchor.constraint(equalTo: registrationLabel.bottomAnchor, constant: 30),
             stack.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20),
-            stack.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -20),
-            stack.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: 20)
+            stack.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -20)
         ])
     }
 
@@ -206,7 +205,7 @@ extension OnboardSignUpViewAlpha {
 
     private func layoutSignUp(in container: UIView) {
         let signUp = signUpButton
-        view.addSubview(signUp)
+        container.addSubview(signUp)
         signUp.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             signUp.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20.0),
@@ -218,7 +217,7 @@ extension OnboardSignUpViewAlpha {
 
     private func layoutSignIn(in container: UIView) {
         let signIn = signInButton
-        view.addSubview(signIn)
+        container.addSubview(signIn)
         signIn.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             signIn.topAnchor.constraint(equalTo: signUpButton.bottomAnchor, constant: 10.0),
