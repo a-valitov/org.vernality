aa1781261e3ddd080372e0a23e60ff0358ff6dda
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-01-23 21:56:13 +0500

    PC-133 use coordinator for SwiftUI

--- Projects/ProfitClub/Presentation/PCOnboard/Demo/Shared/ContentView.swift
+++ Projects/ProfitClub/Presentation/PCOnboard/Demo/Shared/ContentView.swift
@@ -7,13 +7,42 @@
 
 import SwiftUI
 import PCOnboard
+import PCModel
 
 struct ContentView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        return OnboardFactory().make(output: nil).viewController
+    @Environment(\.presentationMode) var presentationMode
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, OnboardModuleOutput {
+        var parent: ContentView
+
+        init(_ parent: ContentView) {
+            self.parent = parent
+        }
+
+        func onboard(module: OnboardModule, didLogin user: PCUser) {
+            print(user)
+        }
+
+        func onboard(module: OnboardModule, didRegister user: PCUser) {
+            print(user)
+        }
     }
 
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ContentView>) -> UIViewController {
+        return OnboardFactory().make(output: context.coordinator).viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<ContentView>) {
+        print(context)
+    }
+}
+
+class Coordinator {
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
     }
 }
