cde1d1af09b689e3fdb19514b20435f4c932528b
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-01-31 14:58:58 +0500

    PC-151 fix crashes

--- Projects/ProfitClub/Model/PCModel/Sources/PCModelParse/PCActionParse.swift
+++ Projects/ProfitClub/Model/PCModel/Sources/PCModelParse/PCActionParse.swift
@@ -34,7 +34,9 @@ public extension PFObject {
         if let statusString = self["statusString"] as? String {
             result.status = PCActionStatus(rawValue: statusString)
         }
-        if let supplier = self["supplier"] as? PFObject, supplier.isDataAvailable {
+        if let actionParse = self as? PCActionParse, let supplier = actionParse.supplier {
+            result.supplier = supplier
+        } else if let supplier = self["supplier"] as? PFObject, supplier.isDataAvailable {
             result.supplier = supplier.pcSupplier.any
         }
         return result
